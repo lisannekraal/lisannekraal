84ce6602f66b0c38d01242b04dc4bc7c28c15f25
lisannekraal <lisannekraal@gmail.com>
2020-11-20 07:39:05 +0100

    add readme

---
+++ README.d
@@ -0,0 +1,12 @@
+### Hi, my name is Lisan ✌
+
+A full-stack web developer with a love for anything geospatial. 
+
+- Focused on JavaScript technologies and a background in environmental sciences, geo-tech (GIS).
+- Tech stack: Node.js, React, Angular, Express, Koa, GraphQL, MongoDB, PostgreSQL and more.
+- Maps are my passion!
+
+Currently working on:
+
+📣 __catcall__ - an interactive map application for existing activist initiatives fighting street harassent. Main features: visualizing their database, platform to report and verify incoming catcalls.
+Status: Refactoring the MVP right now and hope to launch January 2021.
